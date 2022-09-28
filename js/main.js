b8fe42c10dbf4998f406897257fe9e5c34c40ed7
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-28 23:49:20 +0000

    button listener function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // const axios = require('axios')
 
 
-function initialize(){
 
     let header = document.createElement('h1');
     header.innerText = "WeatherApp";
@@ -18,11 +17,6 @@ function initialize(){
     document.body.appendChild(zipform);
     zipform.id = 'formID';
     
-    };
-    
-initialize();
-    console.log(initialize);
-
     let weatherInfo = [];
     let displayState = {
         city:[],
@@ -33,26 +27,32 @@ initialize();
         image:[]  
     };
 
-    // btn.addEventlistener('click', importData);
-
-
-    let zipInfo = document.querySelector('')
-    
-    let API = 'https://api.openweathermap.org/data/2.5/weather?zip=94087,us&appid=2ac06639cc28fd36cdb411b506b44376';
+    btn.addEventListener('click', importData);
+        let API = 'https://api.openweathermap.org/data/2.5/weather?zip=94087,us&appid=2ac06639cc28fd36cdb411b506b44376';
 
     async function importData(url){
+        let zipcode = document.getElementById("formID").value;
+
         
         try{
             let response = await axios.get(url);
             console.log(response.data);
             weatherInfo = await response.data;
-            // displayData(weatherInfo);
+
+            city.innerText = data.name;
+            kTemp.innerText = data.main.temp + '°' + 'K';
+            fTemp.innerText = (data.main.temp - 273.15) * 1.8 + 32 + 'f';
+            cTemp.innerText = (data.main.temp - 273.15);
+            condition = data.weather[0].description;
+            image.innerText = data.weather[0].icon;
             
         }
         catch(error){
+            city.innerText = "Enter a valid zip"
             console.log(error);
         }
     }
+
     
     importData(API);
     
