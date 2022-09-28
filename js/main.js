8a93e8e4ffd5338a23defd59dd5a76a75672a64b
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-28 18:42:25 +0000

    fixed axios issue

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// const axios = require('axios')
+
+
+function initialize(){
+
+    let header = document.createElement('h1');
+    header.innerText = "WeatherApp";
+    document.body.appendChild(header);
+    header.id = 'headerID';
+    
+    let btn = document.createElement("button");
+    btn.innerHTML = "Get Weather";
+    document.body.appendChild(btn);
+    btn.id = 'buttonID';
+    
+    let zipform = document.createElement('input');
+    zipform.placeholder = 'Enter Zipcode';
+    document.body.appendChild(zipform);
+    zipform.id = 'formID';
+    
+    };
+    
+initialize();
+    console.log(initialize);
+    
+    
+    let API = 'https://api.openweathermap.org/data/2.5/weather?zip=40514,us&appid=2ac06639cc28fd36cdb411b506b44376';
+
+    async function importData(url){
+        
+        try{
+            let response = await axios.get(url);
+            console.log(response.data);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    
+importData(API);
+    
