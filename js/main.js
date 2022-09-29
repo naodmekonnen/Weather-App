b4e9ea7e809125f156968bd4ef7df9d31d8c0b34
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-29 13:47:03 +0000

    tried url fix

--- js/main.js
+++ js/main.js
@@ -12,67 +12,35 @@ let zipform = document.createElement('input');
     zipform.placeholder = 'Enter Zipcode';
     document.body.appendChild(zipform);
     zipform.id = 'formID';
+
+let zipInfo = document.getElementById('formID');
+// let button = document.getElementById('buttonID');
+let city = document.getElementById('location');
+let kTemp = document.getElementById('kelvin');
+let cTemp = document.getElementById('celcius');
+let fTemp = document.getElementById('fahrenheit');
+let condition = document.getElementById('description');
+let icon = document.getElementById('image');
+
+btn.addEventListener('click', importData);
+
+    let zipcode = document.getElementById('formID').value;
+    let url = 'https://api.openweathermap.org/data/2.5/weather?zip=94087,us&appid=2ac06639cc28fd36cdb411b506b44376';
+
+    async function importData(){
     
-let city = document.createElement('p');
-    city.innerText = " ";
-    document.body.appendChild(city);
-    city.id = 'location';
-
-let kTemp = document.createElement('p');
-    kTemp.innerText = " ";
-    document.body.appendChild(kTemp);
-    kTemp.id = 'kelvin';
-
-
-let cTemp = document.createElement('p');
-    cTemp.innerText = " ";
-    document.body.appendChild(cTemp);
-    cTemp.id = 'celcius';
-
-let fTemp = document.createElement('p');
-    fTemp.innerText = " ";
-    document.body.appendChild(fTemp);
-    fTemp.id = 'fahrenheit';
-
-let condition = document.createElement('p');
-    condition.innerText = " ";
-    document.body.appendChild(condition);
-    condition.id = 'description';
-
-let icon = document.createElement('img');
-    icon.innerText = " ";
-    document.body.appendChild(icon);
-    icon.id = 'image';
-
-
-
-let displayState = {
-        city:'',
-        kTemp:'',
-        cTemp:'',
-        fTemp:'',
-        condition:'',
-        image:''  
-    };
-
-    btn.addEventListener('click', importData);
-        let API = 'https://api.openweathermap.org/data/2.5/weather?zip=94087,us&appid=2ac06639cc28fd36cdb411b506b44376';
-
-    async function importData(url){
-        let zipcode = document.getElementById("formID").value;
-
-        
         try{
             let response = await axios.get(url);
             console.log(response.data);
-            data = await response.data;
+            data = await response.data
 
-            city.innerText = data.name;
+            city.innerHTML = data.name;
             kTemp.innerText = data.main.temp + '°' + 'K';
             fTemp.innerText = (data.main.temp - 273.15) * 1.8 + 32 + 'F';
             cTemp.innerText = (data.main.temp - 273.15);
             condition = data.weather[0].description;
             image.innerText = data.weather[0].icon;
+            icon.src = 'https://openweathermap.org/img/wn' + data.weather[0].icon + '@2x.png';
             
         }
         catch(error){
@@ -80,5 +48,5 @@ let displayState = {
             console.log(error);
         }
     };
-    importData(API);
+    importData();
     
